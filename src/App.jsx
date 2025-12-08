5ef439030e880135271b1b7d4f431b31ac16f762
Yamamoto Aleksandr <lex@Yamamotos-MacBook-Air.local>
2025-12-09 01:41:55 +0900

    Fix 404: Switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import Dashboard from './pages/Dashboard/Dashboard';
-import BrowseMasters from './pages/BrowseMasters/BrowseMasters';
-import MasterProfile from './pages/MasterProfile/MasterProfile';
-import MyCurriculum from './pages/MyCurriculum/MyCurriculum';
-import Certification from './pages/Certification/Certification';
-import MasterLayout from './layouts/MasterLayout';
-import MasterDashboard from './pages/MasterDashboard/MasterDashboard';
-import MyStudents from './pages/MyStudents/MyStudents';
-import Royalties from './pages/Royalties/Royalties';
-import Treasury from './pages/Treasury/Treasury';
-
-import WalletDetail from './pages/WalletDetail/WalletDetail';
-import Settings from './pages/Settings/Settings';
-
+// ... imports ...
 
 function App() {
     return (
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<Navigate to="/dashboard" replace />} />
@@ -39,7 +26,7 @@ function App() {
                     <Route path="/treasury" element={<Treasury />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
